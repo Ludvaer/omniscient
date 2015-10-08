1205c1cf33cc4f8e944d30bd6aaa2d72a8c067c1
Mikhail Utochkin <mikhailutochkin@yandex.com>
2015-10-08 13:03:57 +0300

    added basic testing

--- test/controllers/pseudo_static_controller_test.rb
+++ test/controllers/pseudo_static_controller_test.rb
@@ -4,6 +4,8 @@ class PseudoStaticControllerTest < ActionController::TestCase
   test "should get welcome" do
     get :welcome
     assert_response :success
+    assert_select "h1", "Welcome"
+    assert_select "title", "Home | Omniscient"
   end
 
 end
