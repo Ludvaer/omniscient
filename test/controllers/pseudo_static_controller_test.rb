8e334249b3eb5d7be6327c611a2842232e9abf8e
Mikhail Utochkin <MikhailUtochkin@yandex.ru>
2015-10-10 16:45:56 +0300

    more sassy styles, minor fixes

--- test/controllers/pseudo_static_controller_test.rb
+++ test/controllers/pseudo_static_controller_test.rb
@@ -4,7 +4,7 @@ class PseudoStaticControllerTest < ActionController::TestCase
   test "should get welcome" do
     get :welcome
     assert_response :success
-    assert_select "h1", "Welcome"
+    assert_select "h2", "Welcome"
     assert_select "title", "Home | Omniscient"
   end
 
