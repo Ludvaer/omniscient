cd633419fc8bb31bb6d93430cbd8e3747484f849
Mikhail Utochkin <MikhailUtochkin@yandex.ru>
2015-10-11 22:30:13 +0300

    somy DRYing and cosmetic enhancements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-APPNAME = 'Omni<span style="font-size:0;"> </span>scent'.html_safe
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,8 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "h2", "Sign Up"
+    assert_select "title", "Sign Up | Omniscient"
   end
 
 end
