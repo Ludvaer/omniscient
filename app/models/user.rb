140cf357fd564a4cc83785dee2360de0e6eceb31
Mikhail Utochkin <MikhailUtochkin@yandex.ru>
2015-10-12 23:37:42 +0300

    fixes in js

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
 	@@sym = [('0'..'9'), ('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
     @@rsa_key = OpenSSL::PKey::RSA.new(2048)
     @@ssl = "asdfasdfasdfasdf"
-
-
+    @@salts = {}
+    @@i = 1
 	def self.key
 		@@rsa_key.public_key
 	end
@@ -16,8 +16,12 @@ class User < ActiveRecord::Base
 	end
 
 	def self.salt
+
 		l = @@sym.length
-		(0...5).map { @@sym[rand(l)] }.join
+		salt = (0...5).map { @@sym[rand(l)] }.join + "#{@@i}"
+		@@i += 1
+		@@salts[salt] = true
+		salt
 	end
 
 
