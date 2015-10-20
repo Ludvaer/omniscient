a93f091da2843067a42bb261c32fa91c7ce810e0
Mikhail Utochkin <MikhailUtochkin@yandex.ru>
2015-10-20 18:17:50 +0300

    added helper for generation messages for selected model property

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     link_to(image_tag(gravatar_url, alt: "gravatar", class: "gravatar"), @user, :class => 'avatar' )
   end
+
+  def errors_for(user, field)
+    errors = user.errors.to_hash(true)[field]
+    errors.map{ |message|  "<div class='error'>#{message}</div>"}.join().html_safe
+  end
 end
